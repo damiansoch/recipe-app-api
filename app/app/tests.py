35916ea8ba8e67bb3365b119cced975c1bc9630e
Damian Socha <damian.socha@amigo.ie>
2024-05-15 14:06:51 +0100

    Adding sample test

---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+"""
+Sample tests
+"""
+from django.test import SimpleTestCase
+from calc import add
+
+
+class CalcTests(SimpleTestCase):
+    def test_add_numbers(self):
+        self.assertEqual(add(3, 8), 11)
