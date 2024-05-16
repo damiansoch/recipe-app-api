f8282e3f5daacbc85df51dfd2a199ff088c016d9
Damian Socha <damian.socha@amigo.ie>
2024-05-16 10:46:44 +0100

    added features error raised when no emai and test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,3 +33,8 @@ class TestModels(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, "sample123")
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test creating user with no email is raised error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "sample123")
