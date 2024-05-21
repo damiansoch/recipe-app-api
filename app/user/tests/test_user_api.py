c1e8abd8fc76b7c3a3c8307a9042b61e95152186
Damian Socha <damian.socha@amigo.ie>
2024-05-21 09:57:10 +0100

    test fix

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -125,7 +125,10 @@ class PrivateUserAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, {
             "name": self.user.name,
-            "email": self.user.email
+            "email": self.user.email,
+            'is_active': self.user.is_active,
+            'is_staff': self.user.is_staff,
+            'is_superuser': self.user.is_superuser,
         })
 
     def test_post_me_not_allowed(self):
